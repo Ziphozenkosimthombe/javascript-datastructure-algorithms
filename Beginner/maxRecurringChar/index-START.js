1c999bb7a13c0b074b9c38d5fae17b57b4b0eeff
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-21 17:17:35 +0200

    maxRecurring Charactor

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -7,7 +7,34 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 function maxRecurringChar(text) {
     // Code goes here
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+        let tempCount = 0;
+        for (let k = 0; k < text.length; k++){
+            if (text[i] === text[k]){
+                tempCount++
+            }
+        }
+        if (tempCount > count){
+            count = tempCount;
+            var char = text[i]
+        }
+    }
+    return char; 
 }
+
+function maxRecurringChar(text){
+    let count = 0;
+    return [...text].reduce((acc, char) => {
+        if (text.split(char).length > count){
+            count = text.split(char).length;
+            return char;
+        } else {
+            return acc;
+        }
+    }, '')
+}
+
 
 
 
