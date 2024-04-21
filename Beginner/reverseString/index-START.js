f10a9d1ca3a0d86922a4dbbad422aab515f64211
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-21 16:01:25 +0200

    reversing the string

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,28 +3,35 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-//======= split method ========
-// function reverseString(text) {
-//     // Code goes here
-//     // const textReversed = text.split('').reverse().join('')
-//     // return textReversed
-//     return text.split('').reverse().join('')
-// }
+//======= chaining method ========
+function reverseString(text) {
+    const textReversed = text.split('').reverse().join('')
+    return textReversed
+}
 
-//=============for of method================
-// function reverseString(text) {
-//     let result = ''
-//     for (let char of text) {
-//         result = char + result
-//     }
-//     return result
 
-//     for (let i = text.length - 1; i >= 0; i--) {
-//         result += text[i]
-//     }
-//     return result
-    
-// }
+//======= chaining method using ES6 ========
+function reverseString(text) {
+    return [...text].reverse().join('')
+}
+
+//=============for loop method using ES6 ================
+function reverseString(text) {
+    let result = ''
+    for (let char of text) {
+        result = char + result
+    }
+    return result
+}
+
+//=======for loop method ========
+function reverseString(text) {
+    let result = ''
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i]
+    }
+    return result
+}
 //======= reduce method is the fastest way to reverse a string ========
 function reverseString(text) {
     return text.split('').reduce((acc, char) => char + acc, '')
