173555cf4e2fe2b24423ceacf01515acf026def8
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-22 20:30:03 +0200

    hammingDistance

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -7,9 +7,18 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    let count = 0
+    if (stringA.length == stringB.length){
+        for(let i = 0; i < stringA.length; i++){
+            // if the characters are not equal, increment the count
+            if(stringA[i].toLowerCase() != stringB[i].toLowerCase()){ 
+                count++;
+            }
+        }
+        return count;
+    }else{
+        throw new Error('Strings do not have equal length');
+    }
 }
-
-
 
 module.exports = hammingDistance
