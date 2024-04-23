559ba6df9bb608c8f39c0b74e98586fca65481db
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-23 19:22:57 +0200

    founding the longest word

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -1,12 +1,27 @@
 /* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
-E.g reverseString('algorithms') // should return 'smhtirogla'
+Given a sentence, return the longest word in the string. E.g
+
+longestWord('Top Shelf Web Development Training on Scotch') 
+//should return 'Development'
 */
 
 
 
 function longestWord(text) {
     // Code goes here
+    let longestWord = text.split(' ')
+    longestWord.forEach((word) => {
+        if (word.length > longestWord.length) {
+            longestWord = word
+        }
+    })
+    return longestWord
+}
+
+function longestWord(text){
+    let name = text.split(' ').sort((a,b) => b.length - a.length)
+    return name[0]
+
 }
 
 
